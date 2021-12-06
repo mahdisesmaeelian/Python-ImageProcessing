9a6dfc285390d23df00192fc3f80ec41f281e7d6
Mahdis Esmaeelian <mahdis9480@gmail.com>
2021-12-06 10:28:04 +0330

    Add files via upload

---
+++ assignment-26-1/ColorSensing.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+video_cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = video_cap.read()
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    myrec = frame[180:300,270:390]
+
+    alpha = 1.5
+    beta = 0
+    
+    if ret == False:
+        break
+ 
+    kernel = np.ones((35,35),np.float32)/1225
+    dst = cv2.filter2D(frame,-1,kernel)
+
+    cv2.rectangle(frame,(270,180), (390,300), (0, 0, 0),2)
+    dst[180:300,270:390] = myrec
+    color_detect_area = cv2.convertScaleAbs(dst[180:300,270:390], alpha=alpha, beta=beta)
+    dst[180:300,270:390] = color_detect_area 
+
+    if  0 < np.average(color_detect_area) <= 60:
+        cv2.putText(dst, "Black", (25, 50), cv2.FONT_HERSHEY_PLAIN,3, (0, 0, 0),3) 
+    elif 60 < np.average(color_detect_area) <= 120:
+        cv2.putText(dst, "Gray", (25, 50), cv2.FONT_HERSHEY_PLAIN,3, (0, 0, 0),3)
+    else:
+        cv2.putText(dst, "White", (25, 50), cv2.FONT_HERSHEY_PLAIN,3, (0, 0, 0),3) 
+
+    cv2.imshow('Camera',dst)
+    cv2.waitKey(1)                 
