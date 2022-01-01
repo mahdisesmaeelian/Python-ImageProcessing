cb2a415a7e6bae9180556e77cba555ef6f9c815d
Mahdis Esmaeelian <mahdis9480@gmail.com>
2022-01-01 17:00:20 +0330

    Update Color Picker.py

--- assignment-29/Color Picker.py
+++ assignment-29/Color Picker.py
@@ -9,36 +9,26 @@ app = QApplication(sys.argv)
 ui = loader.load("Input\color picker.ui", None)
 ui.setWindowTitle("Color Picker")
 
-v_r = 0
-v_g = 0
-v_b = 0
+rcode = ui.redSlider.value()
+gcode = ui.greenSlider.value()
+bcode = ui.blueSlider.value()
 
 def red_slider_changed(_,value):
-    value += 1
     ui.lableR.setText(f"{value}")
-    return value
+    # rcode = ui.redSlider.value()
 
 def green_slider_changed(_,value):
-    value += 1
     ui.lableG.setText(f"{value}")
-    return value
 
 def blue_slider_changed(_,value):
-    value += 1
     ui.lableB.setText(f"{value}")
-    return value
+    
 
-# def label_Color(value1,value2,value3):
-#     ui.labelColor.setText(f"rgb({value1} , {value2} , {value3})")
+ui.redSlider.valueChanged.connect(partial(red_slider_changed,rcode))
+ui.greenSlider.valueChanged.connect(partial(green_slider_changed,gcode))
+ui.blueSlider.valueChanged.connect(partial(blue_slider_changed,bcode))
 
-# value1 = red_slider_changed(v_r,v_r)
-# value2 = green_slider_changed(v_g,v_g)
-# value3 = blue_slider_changed(v_b,v_b)
-# label_Color(value1,value2,value3)
-
-ui.redSlider.valueChanged.connect(partial(red_slider_changed,v_r))
-ui.greenSlider.valueChanged.connect(partial(green_slider_changed,v_g))
-ui.blueSlider.valueChanged.connect(partial(blue_slider_changed,v_b))
+ui.labelColor.setText(f"rgb({rcode} , {gcode} , {bcode })")
 
 ui.show()
 app.exec_()
